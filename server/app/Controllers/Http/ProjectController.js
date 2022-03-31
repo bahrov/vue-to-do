c0de234cda48735676ca6ddcad6f3d2fe629e14e
Maksym Bahrov <m.bahrov@gmail.com>
2022-03-31 14:44:43 +0300

    destroy with exceptions

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -1,22 +1,29 @@
 "use strict";
 
 const Project = use("App/Models/Project");
+const AuthorizationService = use("App/Services/AuthorizationService");
 
 class ProjectController {
   async index({ auth }) {
     const user = await auth.getUser();
-    console.log(user.id);
     return await user.projects().fetch();
   }
 
   async create({ auth, request }) {
     const user = await auth.getUser();
-    console.log(user);
     const { title } = request.all();
-    console.log(title);
     const project = new Project();
     project.title = title;
     await user.projects().save(project);
+    return project;
+  }
+
+  async destroy({ auth, request, params }) {
+    const user = await auth.getUser();
+    const { id } = params;
+    const project = await Project.find(id);
+    AuthorizationService.verifyPermission(project, user);
+    await project.delete();
     return project;
   }
 }
